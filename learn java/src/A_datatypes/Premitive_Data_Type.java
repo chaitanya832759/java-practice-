b37cf13718ce4e3d0b5c271cd34709a829db656f
chaitanya m <chaitanyamalla2001@gmail.com>
2025-04-12 09:02:52 +0530

    premitive datatypes

---
+++ learn java/src/A_datatypes/Premitive_Data_Type.java
@@ -0,0 +1,54 @@
+package A_datatypes;
+
+public class Premitive_Data_Type {
+
+    public static void main(String[] args) {
+
+
+        //premitive data type -- integer,boolean,character
+
+
+        //                  INTEGER type
+
+        //BYTE
+        System.out.println("- Byte ");
+        System.out.println(" size "+ Byte.SIZE);
+        System.out.println(" max "+ Byte.MAX_VALUE +  " min " + Byte.MIN_VALUE);
+
+        //short
+
+        System.out.println("- Short ");
+        System.out.println(" size "+ Short.SIZE);
+        System.out.println(" max "+ Short.MAX_VALUE +  " min " + Short.MIN_VALUE);
+
+        //int
+        System.out.println("- integer ");
+        System.out.println(" size "+ Integer.SIZE);
+        System.out.println(" max "+ Integer.MAX_VALUE +  " min " + Integer.MIN_VALUE);
+
+        //long
+
+        System.out.println("- integer ");
+        System.out.println(" size "+ Long.SIZE);
+        System.out.println(" max "+ Long.MAX_VALUE +  " min " + Long.MIN_VALUE);
+
+        //                decimal
+
+        // float
+
+
+        System.out.println("- float ");
+        System.out.println(" size "+ Float.SIZE);
+        System.out.println(" max "+ Float.MAX_VALUE + " min " + Float.MIN_VALUE);
+
+
+        // double
+
+        System.out.println("- Double ");
+        System.out.println(" size "+ Double.SIZE);
+        System.out.println(" max "+ Double.MAX_VALUE + " min " + Double.MIN_VALUE);
+
+
+    }
+
+}
