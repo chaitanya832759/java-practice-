27c41cc8259ce92ec14669e0a0738d6812b03dd6
chaitanya m <chaitanyamalla2001@gmail.com>
2025-04-12 08:33:57 +0530

    first commit

---
+++ learn java/src/A_datatypes/data_types.java
@@ -0,0 +1,24 @@
+package A_datatypes;
+
+public class data_types {
+
+public static void main(String[] args) {
+
+
+
+    //premitive data type -- integer,boolean,character
+
+
+    //INTEGER type
+
+    //BYTE
+
+    System.out.println("BYTE SIZE"Byte.SIZE);
+
+    B
+
+}
+
+
+
+}
